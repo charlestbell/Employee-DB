f192b3b11b9d79088192995d3eaa054c306cbd22
C.T. Bell <charlestbell@gmail.com>
2021-02-03 23:10:20 -0500

    Added Main Menu functiDn and addDepartment function

--- Index.js
+++ Index.js
@@ -28,16 +28,68 @@ connection.connect((err) => {
 //   console.table(result);
 // });
 // Print joined employee, with role and depeartments to screen.
-connection.query(
-  `SELECT *
-FROM employee
-LEFT JOIN role
-ON employee.role_id = role.id
-LEFT JOIN department
-ON role.department_id = department.id`,
-  (err, result) => {
-    if (err) throw err;
+const joinedTable = async () => {
+  connection.query(
+    `SELECT *
+    FROM employee
+    LEFT JOIN role
+    ON employee.role_id = role.id
+    LEFT JOIN department
+    ON role.department_id = department.id`,
+    (err, result) => {
+      if (err) throw err;
 
-    console.table(result);
-  }
-);
+      console.table(result);
+      mainMenu();
+    }
+  );
+};
+
+//Run the inquirer prompt
+const mainMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: ["Add Department"],
+      },
+    ])
+    .then((response) => {
+      switch (response.choice) {
+        case "Add Department":
+          addDepartment();
+          break;
+
+        default:
+          break;
+      }
+    });
+};
+
+// Add a department to the db
+const addDepartment = () => {
+  //TODO: listDepartments();
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Name the department you would like to add.",
+        name: "newDepartment",
+      },
+    ])
+    .then((response) => {
+      newDept = response.newDepartment;
+      connection.query(
+        `INSERT INTO department (name)
+        VALUES ("${newDept}");`,
+        (err, result) => {
+          if (err) throw err;
+          console.log(`New department called "${newDept}" added successfully`);
+          mainMenu();
+        }
+      );
+    });
+};
+joinedTable();
